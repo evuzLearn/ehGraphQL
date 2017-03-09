e346f65c2065401976714d88af775a7167a5234c
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-03-09 22:37:25 +0100

    GraphQL Schema

--- index.js
+++ index.js
@@ -2,28 +2,62 @@
 
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { graphql, buildSchema } = require('graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLBoolean 
+ } = require('graphql');
 
-const port = process.env.PROT || 3020;
+const port = process.env.PROT || 3020;
 const server = express();
 
-const schema = buildSchema(`
-    type Video {
-        id: ID,
-        title: String,
-        duration: Int,
-        watched: Boolean
+const videoType = new GraphQLObjectType({
+    name: 'Video',
+    title: 'A video on Egghhead.io',
+    fields: {
+        id: {
+            type: GraphQLID,
+            description: 'The ID of the video'
+        },
+        title: {
+            type: GraphQLString,
+            description: 'The title of the video'
+        },
+        duration: {
+            type: GraphQLInt,
+            description: 'The duration of the video (in seconds)'
+        },
+        watched: {
+            type: GraphQLBoolean,
+            description: 'Whether or not the viewer has watched the video'
+        }
     }
-    
-    type Query {
-        video: Video,
-        videos: [Video]
-    }
+});
 
-    type Schema {
-        query: Query
+const queryType = new GraphQLObjectType({
+    name: 'QueryType',
+    description: 'The root query type',
+    fields: {
+        video: {
+            type: videoType,
+            resolve: () => new Promise(resolve => {
+                resolve({
+                    id: 'a',
+                    title: 'GraphQL',
+                    duration: 180,
+                    watched: true
+                });
+            })
+        }
     }
-`);
+});
+
+const schema = new GraphQLSchema({
+    query: queryType
+})
 
 const videoA = {
     id: 3,
@@ -37,25 +71,14 @@ const videoB = {
     id: 4,
     title: 'Ember.js CLI',
     duration: 50,
-    watched: false 
+    watched: false
 }
 
 const videos = [videoA, videoB];
 
-const resolvers = {
-    video: () => ({
-        id: '2',
-        title: 'VideoFoo',
-        duration: 90,
-        watched: false
-    }),
-    videos: () => videos
-}
-
 server.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: true,
-    rootValue: resolvers 
+    graphiql: true
 }))
 
 server.listen(port, () => {
